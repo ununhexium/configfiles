65bf0239ec79f78ad098f828c1c78c78388ae42e
Christophe '116' Loiseau <116@lab0.net>
2019-06-01 19:53:05 +0200

    Fix cpu usage ramdisk detection

--- .config/i3/i3scripts/refresh_cpu_usage.sh
+++ .config/i3/i3scripts/refresh_cpu_usage.sh
@@ -9,7 +9,7 @@ echo "N/A" > touch "$cpu_usage_location"
 
 usage=$(echo "scale=1 ; 100 - "$(mpstat 1 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle') | bc)
 # only write that on tmpfs as it would otherwise incur useless disk write and cells deterioration
-[[ -z $(df "$cpu_usage_location" | tail -n1 | grep '^tmpfs') ]] && echo "Won't write on non tmpfs" && exit 1
+[[ -z $(df $(dirname "$cpu_usage_location") | tail -n1 | grep '^tmpfs') ]] && echo "Won't write on non tmpfs" && exit 1
 
 echo "$usage" > "$cpu_usage_location"
 
