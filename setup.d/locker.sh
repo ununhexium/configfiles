3904a58fef6f3fbce538b484edd86b0844d90bea
Christophe '116' Loiseau <116@lab0.net>
2022-04-24 13:49:09 +0400

    Update config

--- setup.d/locker.sh
+++ setup.d/locker.sh
@@ -2,9 +2,11 @@
 
 needs locker.sh || return
 
+inst_script clone
+
 sudo apt install -y scrot
 
-clone 'https://github.com/ununhexium/hexpxl.git'
+clone 'https://github.com/ununhexium/hexpxl.git' || return
 
 echo "cd $HOME/dev/ununhexium/hexpxl && cargo build --release && cargo install" | bash
 
