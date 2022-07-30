3a8e5e783ca7669a97db50405ea80f528a475b1c
Christophe '116' Loiseau <116@lab0.net>
2022-07-30 22:47:35 +0200

    Install git tools

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo ansible-galaxy collection install community.general
+
 sudo ansible-galaxy install -r ./ansible/requirements.yml
 
 sudo ansible-playbook --tags essentials ./ansible/main.yml
