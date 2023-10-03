0ebd56f2420ce394eab96dc3acf02296087fbb41
Christophe '116' Loiseau <116@lab0.net>
2023-10-03 11:17:59 +0200

    add framebuffer key bindings

--- zsh/include/key_bindings.zsh
+++ zsh/include/key_bindings.zsh
@@ -1,15 +1,29 @@
+
 # Ctrl + ->
 bindkey "^[[1;5C" forward-word
+# framebuffer
+bindkey "^[[C" forward-word
+
 # Ctrl + <-
 bindkey "^[[1;5D" backward-word
+# framebuffer
+bindkey "^[[D" backward-word
+
 # Delete
 bindkey '^[[3~' delete-char
+
 # Home
 bindkey '^[[H' beginning-of-line
 bindkey '^A' beginning-of-line
+# framebuffer
+bindkey '^[[1~' beginning-of-line
+
 # End
 bindkey '^[[F' end-of-line
 bindkey '^E' end-of-line
+# framebuffer
+bindkey '^[[4~' end-of-line
+
 # Backspace
 bindkey '^?' backward-delete-char
 
