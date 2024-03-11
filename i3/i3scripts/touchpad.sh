98b67d3787555a886ce9cc27d478822977d62c3f
Christophe '116' Loiseau <116@lab0.net>
2024-03-11 22:40:12 +0100

    Touchpad locking

---
+++ i3/i3scripts/touchpad.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+declare -i ID
+ID=$(xinput list | grep -Eio '(touchpad|glidepoint)\s*id=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}')
+declare -i STATE
+STATE=$(xinput list-props "$ID" | grep 'Device Enabled' | awk '{print $4}')
+if [ "$STATE" -eq 0 ]
+then
+  echo "<span size='xx-large'>󰟸</span>"
+fi
